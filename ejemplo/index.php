c427aae6b85f8c9db239955d5047ce1902f0f21f
Jose Abad <115423031+JMAbadLopez@users.noreply.github.com>
2023-05-16 17:48:06 +0200

    Update index.php

--- ejemplo/index.php
+++ ejemplo/index.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Música</title>
+</head>
+<body>
+    <h1>Búsqueda de canciones</h1>
+    <form action="index.php" method="get">
+        <input type="text" name="term">
+        <input type="submit" value="Buscar canción">
+    </form>
+
+    <h2>Resultados</h2>
+    <br>
+<!-- Script en PHP -->
 <?php
 
 /** Conexión a la Base de Datos */
@@ -19,3 +37,7 @@ $result = $mysql->query($query);
 while ( $row = $result->fetch_assoc()) {
     printf("<i>%s</i> <strong>%s</strong> <i>%s</i> (%s)<br>", $row['id'], $row['nombre'], $row['artista'], $row['ano_publicacion']);
 }
+
+?>
+</body>
+</html>
